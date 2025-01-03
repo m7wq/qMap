6235c4f822867be3c0417ea6ec396e0909ac5ae8
M7moud_ <seekpotato47@gmail.com>
2025-01-03 20:18:40 +0200

    Initializing

---
+++ api/src/main/java/dev/m7wq/MapAPI.java
@@ -0,0 +1,13 @@
+package dev.m7wq;
+
+/**
+ * Hello world!
+ *
+ */
+public class MapAPI
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
